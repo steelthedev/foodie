023809f7a4e59fced7ed836476e97bebb5a3c23c
Akinwumi Iyanuoluwa Ayomiposi <akinwumikaliyanu@gmail.com>
2024-07-31 22:37:55 +0100

    added sliver app bar

---
+++ lib/components/custom_sliver_app_bar.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class CustomSliverAppBar extends StatelessWidget {
+  final Widget child;
+  final Widget title;
+  const CustomSliverAppBar({
+    super.key,
+    required this.child,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      expandedHeight: 340,
+      collapsedHeight: 120,
+      floating: false,
+      pinned: true,
+      centerTitle: true,
+      actions: [
+        //cart button
+
+        IconButton(onPressed: () {}, icon: const Icon(Icons.shopping_cart))
+      ],
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+      title: Text("Sunset Diner"),
+      flexibleSpace: FlexibleSpaceBar(
+        background: Padding(
+          padding: const EdgeInsets.only(bottom: 50.0),
+          child: child,
+        ),
+        title: title,
+        centerTitle: true,
+        titlePadding: const EdgeInsets.only(left: 0, right: 0, top: 0),
+      ),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:foodie/components/custom_drawer.dart';
+import 'package:foodie/components/custom_sliver_app_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,13 +13,20 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text(
-          "Home",
+      drawer: const CustomDrawer(),
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+          CustomSliverAppBar(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [Text("LOOL")],
+              ),
+              title: Text('title'))
+        ],
+        body: Container(
+          color: Colors.blue,
         ),
       ),
-      drawer: const CustomDrawer(),
     );
   }
 }
